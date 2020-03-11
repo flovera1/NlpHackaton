05de6b323bc3099d3375d0716086107782caf0b4
curiousTauseef <18098441+curiousTauseef@users.noreply.github.com>
2020-03-11 09:21:52 +0100

    script to get top frequent words from phrases corpus

---
+++ scripts/getTopNWordsFromPhrasesCorpus.py
@@ -0,0 +1,35 @@
+#from sklearn import CountVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
+import sys
+import codecs
+
+def get_top_n_words(corpus, n=None):
+    """
+    List the top n words in a vocabulary according to occurrence in a text corpus.
+    
+    get_top_n_words(["I love Python", "Python is a language programming", "Hello world", "I love the world"]) -> 
+    [('python', 2),
+     ('world', 2),
+     ('love', 2),
+     ('hello', 1),
+     ('is', 1),
+     ('programming', 1),
+     ('the', 1),
+     ('language', 1)]
+    """
+    vec = CountVectorizer().fit(corpus)
+    bag_of_words = vec.transform(corpus)
+    sum_words = bag_of_words.sum(axis=0) 
+    words_freq = [(word, sum_words[0, idx]) for word, idx in     vec.vocabulary_.items()]
+    words_freq =sorted(words_freq, key = lambda x: x[1], reverse=True)
+    return words_freq[:n]
+
+
+input_file = sys.argv[1]
+fp = codecs.open(input_file, 'r', 'utf-8')
+
+#cars_for_sell = [line.replace("\n", "") for line in open("cars_for_sell.txt")]
+phrasesCorpusList = [line.replace("\n", "") for line in fp]
+common_words = get_top_n_words(phrasesCorpusList, 300)
+for word, freq in common_words:
+    print(word, freq)
